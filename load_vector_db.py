4414536772bb959f3d6f2a3db70dafaf86b15b2a
SurfinScott <scott.kurowski@10gen.com>
2024-08-29 11:51:30 -0700

    Refactor cleanup

--- load_vector_db.py
+++ load_vector_db.py
@@ -7,6 +7,7 @@
 
     No guarantees or warranties are made in this demo.
 """
+
 # pylint: disable-msg=C0103,C0301,W0621,W0718
 
 import pymongo
@@ -23,10 +24,11 @@ vector_db_name = "demo_db"
 # The expected result should be 6 or 7 clusters and 2 or 3 branch/parent clusters.
 
 TEXTS = [
+    # topic 1
     "Be at one with your power, joy and peace.",
     "Know the flow of the much greater oneness we share.",
     "Let one's superpower be considered choices in the network of life.",
-
+    # topic 2
     "MongoDB Ops Manager",
     "MongoDB Cloud Manager",
     "MongoDB Cloud Manager Backups",
@@ -35,7 +37,7 @@ TEXTS = [
     "MongoDB Atlas Vector Search",
     "MongoDB Atlas Data Lake",
     "MongoDB Enterprise Database Server",
-
+    # topic 3
     "Gödel, Escher, Bach: An Eternal Golden Braid",
     "How Gödel's Theorems Shape Quantum Physics as explored by Wheeler and Hawking",
     "Bach, Johann Sebastian - Six Partitas BWV 825-830 for Piano",
@@ -43,20 +45,20 @@ TEXTS = [
     "Bach's baroque style features recursion or self-referencing iterated functions reminiscent of Escher's nested visuals and Gödelian self-references.",
     "In 1931, Gödel proved the profound duality that formal systems cannot be both self-consistent and complete.",
     "John Von Neumann was able to derive Gödel's 2nd theorem from his 1st before Gödel published it.",
-
+    # topic 4
     "My cat is a fun black and white half-sized tuxedo.",
     "Some people prefer the company of dogs instead of cats.",
     "My friend has a large saltwater aquarium with colorful and exotic tropical fish.",
     "My clever dog opens locked windows and doors to follow me.",
-
+    # topic 5
     "Mesopotamian tablets record a fantastic version of human history.",
     "North American burial mounds often held deceased local royal families.",
     "Mayan pyramids predated most Aztec pyramids.",
     "The Aztecs Quetzalcoatl closely resembles the Egyptian god Thoth.",
 ]
 
-VECTOR_COLLECTION = "demo_target_vectors" # destination MongoDB Atlas vector collection
-EMBEDDING_MODEL = "text-embedding-ada-002" # embed texts as vectors using this model
+VECTOR_COLLECTION = "demo_target_vectors"  # destination MongoDB Atlas vector collection
+EMBEDDING_MODEL = "text-embedding-ada-002"  # embed texts as vectors using this model
 
 mongo_client = pymongo.MongoClient(vector_mongo_uri)
 vector_db = mongo_client[vector_db_name]
@@ -65,7 +67,7 @@ model_client = OpenAI()
 
 
 def get_embeddings_vectors(text_list, model=EMBEDDING_MODEL) -> list[list[float]]:
-    """ get embedding vectors and load into a list of float lists. """
+    """get embedding vectors and load into a list of float lists."""
     response = model_client.embeddings.create(
         input=text_list,
         model=model,
@@ -77,17 +79,22 @@ def get_embeddings_vectors(text_list, model=EMBEDDING_MODEL) -> list[list[float]
 
 
 def load_demo_vectors(
-        vector_db_name=vector_db_name,
-        VECTOR_COLLECTION=VECTOR_COLLECTION,
-        EMBEDDING_MODEL=EMBEDDING_MODEL):
-    """ Load TEXTS and their embedding vectors. """    
+    vector_db_name=vector_db_name,
+    VECTOR_COLLECTION=VECTOR_COLLECTION,
+    EMBEDDING_MODEL=EMBEDDING_MODEL,
+):
+    """Load TEXTS and their embedding vectors."""
     # clean slate
     res = vector_db[VECTOR_COLLECTION].delete_many({})
-    print(f"Removed {res.deleted_count} documents in {vector_db_name}.{VECTOR_COLLECTION} before loading demo vectors")
-    vectors = get_embeddings_vectors(TEXTS,model=EMBEDDING_MODEL)
+    print(
+        f"Removed {res.deleted_count} documents in {vector_db_name}.{VECTOR_COLLECTION} before loading demo vectors"
+    )
+    vectors = get_embeddings_vectors(TEXTS, model=EMBEDDING_MODEL)
     assert len(vectors) == len(TEXTS)
     docs = []
-    docs.extend([{ "text": text, "embedding": vectors[ix] } for ix, text in enumerate(TEXTS)])
+    docs.extend(
+        [{"text": text, "embedding": vectors[ix]} for ix, text in enumerate(TEXTS)]
+    )
     res = vector_db[VECTOR_COLLECTION].insert_many(docs)
     assert len(res.inserted_ids) == len(TEXTS)
     print(f"Loaded {vector_db_name}.{VECTOR_COLLECTION} with {len(TEXTS)} demo vectors")
